87c1866b62d39eeb5a3a779d24f3a7fcc1272357
kc <kclui.nus@gmail.com>
2026-06-24 23:53:15 +0800

    Add card affinity favicon

---
+++ app/icon.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#020617",
+          borderRadius: "24%",
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src="/card-affinity-logo.jpeg"
+          alt="Card Affinity logo"
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      </div>
+    ),
+    size,
+  );
+}
